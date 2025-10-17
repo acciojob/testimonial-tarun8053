784d7d88be5256b4e4735f0571ad9f329a48b7ea
Tarun <thakurtarun1012@gmail.com>
2025-10-17 14:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+
+CSS
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+⌄
+⌄
+⌄
+.container {
+    padding: 10px;
+    background-color: #f5f5f5;
+}
+
+.p1 {
+    background-color: white;
+    padding: 10px;
+    border: 2px solid black;
+    margin: 10px 0;
+}
+
+.autor {
+    text-align: right;
+    font-style: italic;
+}
+
+
+Javascript
+
+1
+2
